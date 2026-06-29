df9ba8acf1a3a643348bf6676568ec767f84d32d
Adeniyi Feranmi <you@example.com>
2026-06-29 10:06:12 +0100

    all

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/",
+
+  build: {
+    outDir: "dist",
+  },
 });
